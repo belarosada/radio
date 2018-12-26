748e21df91ed01978f4cd871b0300202dbaf68b2
Bela Rosada <belrsd@gmail.com>
2018-12-26 11:25:01 +0800

    Fixing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
+
+        Schema::defaultStringLength(191);
     }
 
     /**
